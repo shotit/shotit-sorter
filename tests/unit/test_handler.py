11db2e5eb48d241e105a1bbca96b7521937938a4
Leslie Wong <79917148leslie@gmail.com>
2023-03-30 11:04:26 +0800

    Understand pytest fixture right

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -9,19 +9,21 @@ from main import app
 
 
 def run_server():
-    uvicorn.run(app, host="0.0.0.0", port=19532)
+    uvicorn.run(app, host="127.0.0.1", port=19532)
 
 
 @pytest.fixture
 def server():
     proc = Process(target=run_server, args=(), daemon=True)
     proc.start()
-    time.sleep(5)
+    print("Uvicorn server sets up")
     yield
     proc.kill()  # Cleanup after test
 
 
-def test_main_sort():
+def test_main_sort(server):
+    time.sleep(60)
+    print("Uvicorn server does set up", server)
     data = {"candidates": '{"candidates": [{"image": "https://i.ibb.co/JCj5T41/big-buck-bunny-1.png"},{"image": "https://i.ibb.co/HHJPP3R/big-buck-bunny-2.png"},{"image": "https://i.ibb.co/LPR0gb7/big-buck-bunny-3.png"},{"image": "https://i.ibb.co/qnwfks9/big-buck-bunny-4.png"},{"image": "https://i.ibb.co/56nvNHD/big-buck-bunny-5.png"},{"image": "https://i.ibb.co/jM3657F/big-buck-bunny-6.png"},{"image": "https://i.ibb.co/ZhDQshx/big-buck-bunny-7.png"},{"image": "https://i.ibb.co/0h5gD7y/big-buck-bunny-8.png"},{"image": "https://i.ibb.co/XV54Rk7/big-buck-bunny-9.png"},{"image": "https://i.ibb.co/KGwVkqy/big-buck-bunny-10.png"}, {"image": "https://i.ibb.co/J7v6p24/big-buck-bunny-11.png"}]}'}
     files = [
         ("target", ("big_buck_bunny_10.png", open(os.path.join(

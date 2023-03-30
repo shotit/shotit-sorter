1bea6646154f58e1c520c814f854feeb2aa148d2
Leslie Wong <79917148leslie@gmail.com>
2023-03-30 14:06:01 +0800

    Move time.sleep again

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -17,13 +17,13 @@ def sorter_server():
     proc = Process(target=run_sorter_server, args=(), daemon=True)
     proc.start()
     print("Uvicorn server sets up")
+    time.sleep(60)
     yield
     proc.kill()  # Cleanup after test
 
 
 def test_main_sort(sorter_server):
     print("Uvicorn server does set up", sorter_server)
-    time.sleep(60)
     data = {"candidates": '{"candidates": [{"image": "https://i.ibb.co/JCj5T41/big-buck-bunny-1.png"},{"image": "https://i.ibb.co/HHJPP3R/big-buck-bunny-2.png"},{"image": "https://i.ibb.co/LPR0gb7/big-buck-bunny-3.png"},{"image": "https://i.ibb.co/qnwfks9/big-buck-bunny-4.png"},{"image": "https://i.ibb.co/56nvNHD/big-buck-bunny-5.png"},{"image": "https://i.ibb.co/jM3657F/big-buck-bunny-6.png"},{"image": "https://i.ibb.co/ZhDQshx/big-buck-bunny-7.png"},{"image": "https://i.ibb.co/0h5gD7y/big-buck-bunny-8.png"},{"image": "https://i.ibb.co/XV54Rk7/big-buck-bunny-9.png"},{"image": "https://i.ibb.co/KGwVkqy/big-buck-bunny-10.png"}, {"image": "https://i.ibb.co/J7v6p24/big-buck-bunny-11.png"}]}'}
     files = [
         ("target", ("big_buck_bunny_10.png", open(os.path.join(

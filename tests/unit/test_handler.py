fe11040911aa270e8dd82ff28ecfc1731d4c532e
Leslie Wong <79917148leslie@gmail.com>
2023-04-01 13:54:47 +0800

    Code coverage done right

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -62,7 +62,8 @@ def test_main_sort_normal(sorter_server):
 
 def test_main_sort_single(sorter_server):
     print("Uvicorn server does set up", sorter_server)
-    data = {"candidates": '{"candidates": [{"image": "https://i.ibb.co/JCj5T41/big-buck-bunny-1.png"},{"image": "https://i.ibb.co/HHJPP3R/big-buck-bunny-2.png"},{"image": "https://i.ibb.co/LPR0gb7/big-buck-bunny-3.png"},{"image": "https://i.ibb.co/qnwfks9/big-buck-bunny-4.png"},{"image": "https://i.ibb.co/56nvNHD/big-buck-bunny-5.png"},{"image": "https://i.ibb.co/jM3657F/big-buck-bunny-6.png"},{"image": "https://i.ibb.co/ZhDQshx/big-buck-bunny-7.png"},{"image": "https://i.ibb.co/0h5gD7y/big-buck-bunny-8.png"},{"image": "https://i.ibb.co/XV54Rk7/big-buck-bunny-9.png"},{"image": "https://i.ibb.co/KGwVkqy/big-buck-bunny-10.png"}, {"image": "https://i.ibb.co/J7v6p24/big-buck-bunny-11.png"}]}'}
+    data = {
+        "candidates": '{"candidates": [{"image": "https://i.ibb.co/J7v6p24/big-buck-bunny-10.png"}]}'}
     files = [
         ("target", ("big_buck_bunny_10.png", open(os.path.join(
             os.path.abspath('.'), "tests", "unit", "image", "big_buck_bunny_10.png"), "rb"), "image/png"))
